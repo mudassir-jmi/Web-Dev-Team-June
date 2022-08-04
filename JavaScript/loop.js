ab637fbc7cb5beaf4dbbed387b8619bdfdf9b4a0
Md Mudassir Akhter <mudassirakhter420@gmail.com>
2022-08-04 17:20:50 +0530

    04-aug loop

---
+++ JavaScript/loop.js
@@ -0,0 +1,81 @@
+// While loop
+
+// let counter = 1;
+// while(counter <=10) {
+//     document.write("xyz" + "<br>");
+//     // counter = counter + 1
+//     counter++;  // Entry control loop
+// }
+
+// do While loop
+
+// let counter = 12;
+// do {
+//     document.write("hello");
+//     counter++;
+// }while(counter <= 10);  // exist control loop
+
+
+// for loop 
+
+// for(let counter = 1; counter <=10; counter++ ) {
+//     document.write("hello world");
+// }
+
+// break and contiue
+
+// for(let counter = 1; counter <=10; counter++ ) {
+//     if(counter == 5) {
+//         continue;
+//     }
+//     document.write(counter);
+//     document.write('<br>');
+// }
+
+// alert , prompt and confirm
+
+// alert("welcome");
+
+//  let name = prompt("Enter your name");
+  
+//  if(name !=null) {
+//     document.write(`your name is ${name}`);
+//  }else {
+//     document.write("name is blank");
+//  }
+
+// confirm 
+
+// let response = confirm("r u sure , you want to delete");
+
+// if(response) {
+//     document.write("deleted");
+// }else {
+//     document.write("not deleted");
+// }
+
+//  Type conversion
+
+// let type = "5" + 5;
+// document.write(type);
+// document.write(typeof type);
+
+// let type = "5" * "hi";
+// document.write(type);  //NaN
+// document.write(typeof type); 
+
+// type converstion using function
+
+let type = 2;
+// document.write(typeof type);
+
+let type1 = Boolean(type);
+document.write(typeof type1);
+
+
+
+
+
+
+
+
