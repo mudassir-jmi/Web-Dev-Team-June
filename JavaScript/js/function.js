7c321c65289d063aab5ea60b93d7778639770439
Md Mudassir Akhter <mudassirakhter420@gmail.com>
2022-08-16 16:51:13 +0530

    16 -Aug

--- JavaScript/js/function.js
+++ JavaScript/js/function.js
@@ -15,17 +15,16 @@
 // 4. fucntion names are case-sensitive in JavaScript 🚀
 
 // CREATE FUNCTION 🚀 😄
-function display() {  // declare
-   console.log("Mudassir");
-}
-
-display(); // call
+// function display() {  // declare
+//    console.log("Mudassir");
+// }
+// display(); // call
 
 // ADD TWO NUMBER USING FUNCTION 🛩️
-function add(num1, num2) {
-    console.log(num1 + num2);
-}
-add(4,5); // call
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+// add(4,5); // call
 
 // fUNCTION FOR login user 🦸
 // function login(username, password) {
@@ -38,11 +37,49 @@ add(4,5); // call
 
 // 2 x 1 = 2
 
-function table(num) {
-   for(i = 1; i<=10; i++) {
-      document.write(`${num} x ${i} = ${num * i} `);
-      document.write("<br>");
-   }
-}
+// function table(num) {
+//    for(i = 1; i<=10; i++) {
+//       document.write(`${num} x ${i} = ${num * i} `);
+//       document.write("<br>");
+//    }
+// }
+// table(9); // call
 
-table(9); // call
+// 16 Aug 2022 ---- 4 PM  ⏲️
+
+// Covert string To UpperCase using Function 🚀
+// function upperCase (str) {
+//    console.log(str.toUpperCase());
+// }
+// upperCase('javascript');  // call
+// function lowerCase (str) {
+//    return str.toLowerCase();
+// }
+// const result = lowerCase('javascript');
+// console.log(result);
+
+// Arguments Object In Function  🚀😈
+// function add() {
+//    if(arguments.length == 0) {
+//       console.log("no agrument paas");
+//    }else {
+//       let sum = 0;
+//       for(let i = 0; i<arguments.length; i++) {
+//          sum = sum + arguments[i];
+//          // sum+=arguments[i];
+//       }
+//       console.log(sum);
+//    }
+// }
+// add(5,5,6,7);  // call
+
+// Global Variable and Local Variable
+
+let fruit = "mango";   // global variable
+function sum () {
+   let result = 22;  // local variable
+   console.log(fruit);
+}
+// console.log(fruit);
+// console.log(result); 
+sum();
