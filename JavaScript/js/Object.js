2d9f078099edcbddcd32002a7f535ba6d886eeb5
Md Mudassir Akhter <mudassirakhter420@gmail.com>
2022-09-02 17:01:27 +0530

    2 sep object

--- JavaScript/js/Object.js
+++ JavaScript/js/Object.js
@@ -35,16 +35,60 @@
 // person.greet();
 
 // ES6 🚀 👇
-let person = {
-    name : "xyz",
-    age : 20,
-    roll: 12,
-    sayHello() {
-        console.log("hello world");
-    }
-};
+// let person = {
+//     name : "xyz",
+//     age : 20,
+//     roll: 12,
+//     sayHello() {
+//         console.log("hello world");
+//     }
+// };
+// person.sayHello(); // call
 
-person.sayHello(); // call
+// 02/09/2022............Friday 😸
 
+// This Keyword 🚀 👇 
+// this keyword refers to the object it belongs to. 👈
+// this keyword use within the object instead of object name 👈
+
+// let person = {
+//     fullName : "Akhter",
+//     age: 20,
+//     display() {
+//        console.log('Hello i am ' 
+//        +this.fullName 
+//        +' and i have a ' 
+//        + cloth.brand + " cloth");
+//     //    console.log(this);
+//     }
+// }
+
+// let cloth = {
+//     brand: 'sparkey',
+//     type: 'Jeans',
+// }
+// person.display();  // call
+// Math Object 🚀 👇
+// Home Work
+// Date Object 🚀 👇
+// let x = new Date();
+// console.log(x);  // current date
+// console.log(x.getMonth());
+// console.log(x.getFullYear());
+// console.log(x.getHours());
+// console.log(x.getMinutes());
+// console.log(x.getDay());
+
+// New Keyword 🚀 👇
+// let person = {
+//     name: "xyz",
+//     age : 20,
+// }
+// console.log(person.name);
+let person = new Object();
+person.name = "Akhter";
+person.age = 20;
+person.roll = 12;
+console.log(`${person.name}`);
 
 
