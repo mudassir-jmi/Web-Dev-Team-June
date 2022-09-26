3e560f43c31e27606a3d8e5bb1eb785066d9a8e6
Md Mudassir Akhter <mudassirakhter420@gmail.com>
2022-09-26 16:50:59 +0530

    dom end    26/09/22

---
+++ JavaScript/DOM/DOM_Events/todo.js
@@ -0,0 +1,23 @@
+   // By defualt submit  button is disabled
+            document.querySelector('#submit').disabled = true;
+            // When type something in input field button is enable
+            document.querySelector('#task').onkeyup = () => {
+                if(document.querySelector('#task').value.length > 0) {
+                 document.querySelector('#submit').disabled= false;
+                }else {
+                     document.querySelector('#submit').disabled= true;
+                }
+                
+            }
+            document.querySelector('form').onsubmit = () => {
+                const task = document.querySelector('#task').value;
+                // console.log(task);
+                const li = document.createElement('li');
+                 li.innerHTML = task;
+                 document.querySelector('#tasks').append(li);
+                 // clear previous inputs
+                 document.querySelector('#task').value = '';
+                 document.querySelector('#submit').disabled = true;
+                // stop form from submitting 
+                return false;
+            }
